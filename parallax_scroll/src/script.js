0d6b791b2b4d98091baa29212f004b2a100ff873
Lisa <lisacjheinhold@gmail.com>
2024-02-24 15:26:15 -0700

    add comments

--- parallax_scroll/src/script.js
+++ parallax_scroll/src/script.js
@@ -1,8 +1,12 @@
+// getting elements by id
 let sun = document.getElementById('sun')
 let title = document.getElementById('title')
 
+// adding event listener function to window
 window.addEventListener('scroll', () => {
+  // set variable for scrolling
   let value = window.scrollY;
+  // setting style properties for scrolling speed
   title.style.top = value * 0.75 + 'px';
   sun.style.top = value * 0.25 + 'px';
 });
